64981d17e3aeb26715ce1e68e06d879003174600
BallaMoussa9 <ballamoussasinayoko@gmail.com>
2024-09-14 15:18:48 +0000

    mise à jour des dossiers dataconncetion,main,studentui

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,10 @@
 package main;
 
+import ui.StudentUI;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        // Crée et affiche l'interface utilisateur
+        new StudentUI();
+    }
 }
